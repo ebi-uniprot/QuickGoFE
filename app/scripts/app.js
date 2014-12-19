54cc8c9296916a5fccb75d0b887fb45a36345405
twardell <TonyWardell@users.noreply.github.com>
2014-12-19 17:36:41 +0000

    Recreate Filtering using AngularJS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -125,14 +125,84 @@ app.controller('AnnotationListCtrl', function($scope) {
           {'taxId':'9031', 'title':'Gallus gallus'},
           {'taxId':'9913', 'title':'Bos taurus'}]
 
-      $scope.showCommonTaxon=function(taxon, filter){
-        //if(filter!="" && filter!="false")
-        //{
-        //  if(taxon.taxId=='9606'|taxon.taxId=='10090'|taxon.taxId=='10116')
-        //    return true;
-        //  return false;
-        //}
-        return false;
+      $scope.showCommonTaxon=function(taxon, showAll){
+
+        if($scope.showAll==="" || $scope.showAll==='false')
+        {
+          //Only show selected
+          if(taxon.taxId=='9606'|taxon.taxId=='10090'|taxon.taxId=='10116')
+            return true;
+          return false;
+        }
+        return true;
       }
+
+
+      //$scope.appliedFilters=[
+      //  {'key':'taxon', 'ids': [{'id' : '9606'}, {'id': '10116'} ]},
+      //  {'key':'protein ', 'ids': [{'id' : '123456'} ]},
+      //  {'key':'goId  ', 'ids': [{'id' : 'GO:0008270'} ]}
+      //];
+
+
+    $scope.appliedFilters=[
+      {'key':'taxon', 'id' : '9606'},
+      {'key':'taxon', 'id': '10116'},
+      {'key':'protein ', 'id' : '123456'},
+      {'key':'goId', 'id' : 'GO:0008270'}
+    ];
+
+
+  //Remove filter from applied filters
+  //parm: filter.key-filterValue
+      $scope.removeFilter=function(filter) {
+        var filterLen=-1;
+        var i;
+
+        for (i = 0,  filterLen = $scope.appliedFilters.length; i < filterLen; i++) {
+
+          if($scope.appliedFilters[i].key==filter.key) {
+
+            if ($scope.appliedFilters[i].id == filter.id) {
+              delete $scope.appliedFilters[i];
+
+            }
+          }
+        }
+      }
+
+  //$scope.removeFilter=function(filterKey, filterValue) {
+  //  console.log('filterValue.id=' + filterValue.id);
+  //  var filterLen=-1;
+  //  var valuesLen=-1
+  //  var i;
+  //  var j;
+  //  console.log($scope.appliedFilters.length);
+  //  for (i = 0,  filterLen = $scope.appliedFilters.length; i < filterLen; i++) {
+  //
+  //    //Find filter type
+  //
+  //    if($scope.appliedFilters[i].key==filterKey){
+  //      console.log('here2');
+  //      if($scope.appliedFilters[i].ids.length==1){
+  //
+  //        //Delete all the applied filters for the type if only one type exists
+  //        delete $scope.appliedFilters[i];
+  //
+  //      }else{
+  //
+  //        //loop through all values and delete what meets the criteria for value
+  //        for (j = 0,  valuesLen =$scope.appliedFilters[i].ids.length; j < valuesLen; j++) {
+  //
+  //          console.log('asda!' + $scope.appliedFilters[i].ids[j].id)
+  //
+  //          if ($scope.appliedFilters[i].ids[j].id == filterValue.id) {
+  //            delete $scope.appliedFilters[i].ids[j];
+  //          }
+  //        }
+  //      }
+  //    }
+  //  }
+  //}
 
 });
