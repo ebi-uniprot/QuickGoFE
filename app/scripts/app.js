4e1bb054b77ebed4bf2cbed28f17265e7a9f9a0f
twardell <twardell@ebi.ac.uk>
2014-12-22 10:47:46 +0000

    Change to removeFilter

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -138,13 +138,6 @@ app.controller('AnnotationListCtrl', function($scope) {
       }
 
 
-      //$scope.appliedFilters=[
-      //  {'key':'taxon', 'ids': [{'id' : '9606'}, {'id': '10116'} ]},
-      //  {'key':'protein ', 'ids': [{'id' : '123456'} ]},
-      //  {'key':'goId  ', 'ids': [{'id' : 'GO:0008270'} ]}
-      //];
-
-
     $scope.appliedFilters=[
       {'key':'taxon', 'id' : '9606'},
       {'key':'taxon', 'id': '10116'},
@@ -156,12 +149,12 @@ app.controller('AnnotationListCtrl', function($scope) {
   //Remove filter from applied filters
   //parm: filter.key-filterValue
       $scope.removeFilter=function(filter) {
-        var filterLen=-1;
+        var filterLen = -1;
         var i;
 
-        for (i = 0,  filterLen = $scope.appliedFilters.length; i < filterLen; i++) {
+        for (i = 0, filterLen = $scope.appliedFilters.length; i < filterLen; i++) {
 
-          if($scope.appliedFilters[i].key==filter.key) {
+          if ($scope.appliedFilters[i].key == filter.key) {
 
             if ($scope.appliedFilters[i].id == filter.id) {
               delete $scope.appliedFilters[i];
@@ -170,39 +163,5 @@ app.controller('AnnotationListCtrl', function($scope) {
           }
         }
       }
-
-  //$scope.removeFilter=function(filterKey, filterValue) {
-  //  console.log('filterValue.id=' + filterValue.id);
-  //  var filterLen=-1;
-  //  var valuesLen=-1
-  //  var i;
-  //  var j;
-  //  console.log($scope.appliedFilters.length);
-  //  for (i = 0,  filterLen = $scope.appliedFilters.length; i < filterLen; i++) {
-  //
-  //    //Find filter type
-  //
-  //    if($scope.appliedFilters[i].key==filterKey){
-  //      console.log('here2');
-  //      if($scope.appliedFilters[i].ids.length==1){
-  //
-  //        //Delete all the applied filters for the type if only one type exists
-  //        delete $scope.appliedFilters[i];
-  //
-  //      }else{
-  //
-  //        //loop through all values and delete what meets the criteria for value
-  //        for (j = 0,  valuesLen =$scope.appliedFilters[i].ids.length; j < valuesLen; j++) {
-  //
-  //          console.log('asda!' + $scope.appliedFilters[i].ids[j].id)
-  //
-  //          if ($scope.appliedFilters[i].ids[j].id == filterValue.id) {
-  //            delete $scope.appliedFilters[i].ids[j];
-  //          }
-  //        }
-  //      }
-  //    }
-  //  }
-  //}
 
 });
