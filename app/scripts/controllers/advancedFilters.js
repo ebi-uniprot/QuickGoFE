14517ffb05d936c555069979bfc2eb90f807acb4
twardell <twardell@ebi.ac.uk>
2015-03-06 17:00:19 +0000

    Update to Advanced Filters dialog box.

--- app/scripts/controllers/advancedFilters.js
+++ app/scripts/controllers/advancedFilters.js
@@ -1,10 +1,30 @@
 /**
  * Created by twardell on 04/03/2015.
  */
-app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance ) {
+app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance, basketService, evidencetypes, withDBs ) {
 
+  /**
+   * Basket items are used by the go identifer tab
+   */
+  $scope.basketItems = basketService.getItems();
 
+  /**
+   * Get Evidence Types
+   */
+  var resultET = evidencetypes.query();
+  resultET.$promise.then(function(data){
+    $scope.evidenceTypes = data;
+    console.log("Got Evidence Types", $scope.evidenceTypes);
+  });
 
+  /**
+   * Get With DBs
+   */
+  var resultWDB = withDBs.query();
+  resultWDB.$promise.then(function(data){
+    $scope.withDBs = data;
+    console.log("Got With DBs", $scope.withDBs);
+  });
 
   /**
    * Close window
