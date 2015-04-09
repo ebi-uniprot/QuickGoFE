7c41b0a5bea8fed9aa28822fb13f9bd54493abd2
twardell <twardell@ebi.ac.uk>
2015-04-09 15:18:06 +0100

    Add some form processing to the basket add functionality (submit)

--- app/scripts/controllers/basket.js
+++ app/scripts/controllers/basket.js
@@ -2,7 +2,7 @@
  * Created by twardell on 27/01/2015.
  */
 
-app.controller('BasketCtrl', function($scope, $log, $modalInstance, $location, basketService ) {
+app.controller('BasketCtrl', function($scope, $log, $modalInstance, $location, basketService, term ) {
 
 
   $scope.basketItems = basketService.getItems();
@@ -19,6 +19,53 @@ app.controller('BasketCtrl', function($scope, $log, $modalInstance, $location, b
     $scope.$emit('basketUpdate', basketService.basketQuantity());
 
   }
+
+
+  /**
+   * Iterate through ALL selectable elements
+   * Create a list of selected ones
+   * Pass that list to the filtering service.
+   */
+  $scope.submit = function(basketModal){
+
+
+    console.log("In the basket modal ");
+    // console.log("get the text area value", advancedFilters);
+    //filteringService.setFilters(advancedFilters);
+
+    //Tell parent page this value has been updated.
+
+    //$scope.$emit('filtersUpdate', advancedFilters);
+
+    //Now go back to the annotation list
+    //$modalInstance.dismiss('cancel');
+    //$location.path("annotations");
+
+    //parse the contents of the textbox
+    //iterate through elements found
+    //look up GO Term based on the id
+    //and save the resulting data to the basket.
+
+    basketModal.text.replace(/\t/g, " ");   //tab
+    basketModal.text.replace(/\r\n/g, " "); //Windows
+    basketModal.text.replace(/\n/g, " ");   //The linux
+    var goIds = basketModal.text.split(" ");
+
+    var k =-1;
+    for(k=0; k<goIds.size; k++){
+
+      var termData=term.query({termId : goIds[k]});
+      console.log(termData);
+    }
+
+    //var basketItem = {termId:termId, name:termName};
+    //console.log(basketService.addBasketItem(basketItem));
+    //$scope.countBasket = basketService.getItems().length;
+
+  }
+
+
+
 
   $scope.term = function(goId){
     $modalInstance.dismiss('forward');
