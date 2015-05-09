5153f48cfa4cbaf79aebbea447c425de8dae7d26
TonyWardell <tonyandamie@yahoo.co.uk>
2015-05-09 18:02:22 +0100

    Bookmarkable link functionality

---
+++ app/scripts/controllers/bookmark.js
@@ -0,0 +1,10 @@
+/**
+ * Created by twardell on 09/05/2015.
+ */
+app.controller('BasketCtrl', function($scope, $location, filteringService ) {
+
+  //Parse the query parameters and forward to the annotation page after populating the filtering service values
+
+
+
+});
