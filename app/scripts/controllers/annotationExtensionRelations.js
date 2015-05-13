0f04fdcd2a1d5bce03fe1533ae9fddbac020e54e
TonyWardell <tonyandamie@yahoo.co.uk>
2015-05-13 17:44:27 +0100

    First commit with copy of code from Beta.

---
+++ app/scripts/controllers/annotationExtensionRelations.js
@@ -0,0 +1,20 @@
+/**
+ * Created by twardell on 13/05/2015.
+ */
+
+app.controller('AnnotationExtensionRelationsCtrl', function() {
+
+
+  $(document).ready(
+    function() {
+      $("#cytoscapeweb").html('Retrieving graph data...');
+      $('.window .close').click(function(e) {
+        //Cancel the link behavior
+        e.preventDefault();
+        $('.window').hide();
+      });
+
+      drawOntology(${data});
+    });
+
+});
