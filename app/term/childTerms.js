08d48f5035483652fc7ec7441b643dd17426540e
TonyWardell <tonyandamie@yahoo.co.uk>
2015-05-18 15:11:03 +0100

    Get the data and make it work properly

--- app/term/childTerms.js
+++ app/term/childTerms.js
@@ -1,19 +1,28 @@
 /**
  * Created by twardell on 10/02/2015.
  */
-app.controller('ChildTermsGraphCtrl', function($scope, $http, targetDomainAndPort) {
+app.controller('ChildTermsCtrl', function($scope, $http, targetDomainAndPort) {
+
+  console.log("In child terms page");
+
+  var pathVals =$location.path().split("/");
+  //var termId=pathVals[(pathVals.length-1)];
+
+  console.log("In AncestorsGraphCtrl");
+  $scope.isLoading = 1;
+  $scope.term = {};
+  $scope.term.id =pathVals[(pathVals.length-1)];
+  $scope.imageSource="";
 
 
   var formattedURL=targetDomainAndPort+'/ws/ontologyGraph/';
-  var termId='GO:0003824';
 
   /**
    * Get Term Data from WS
    */
-  //$http.get(formattedURL+termId).success(function(data) {
-  //  $scope.termGraphImageSrc = data;
-  //  console.log("termGraphImageSrc",$scope.termGraphImageSrc);
-  //
-  //});
+  $http.get(formattedURL+$scope.term.id).success(function(data) {
+    console.log("Got term data", data);
+
+  });
 
 });
