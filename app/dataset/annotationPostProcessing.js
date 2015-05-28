6235bf1428af3fca63128912986b3c25e2b93ec7
TonyWardell <tonyandamie@yahoo.co.uk>
2015-05-28 14:55:15 +0100

    Add add to basket and show basket functionality.

--- app/dataset/annotationPostProcessing.js
+++ app/dataset/annotationPostProcessing.js
@@ -1,7 +1,7 @@
 /**
  * Created by twardell on 03/03/2015.
  */
-app.controller('AnnotationPostProcessingCtrl', function($scope, annotationPostProcessing) {
+app.controller('AnnotationPostProcessingCtrl', function($scope, $modal, basketService, annotationPostProcessing) {
 
   $scope.annotationPostProcessing ={};
   $scope.myPromise = annotationPostProcessing.query({});
@@ -9,4 +9,60 @@ app.controller('AnnotationPostProcessingCtrl', function($scope, annotationPostPr
     $scope.annotationPostProcessing = data;
     console.log("Annotation post processing", $scope.annotationPostProcessing);
   });
+
+
+  /**
+   *  Basket Functionality -------------------------------------------------------------------------------------------/
+   */
+
+  /**
+   * Load basket quantity on page load.
+   */
+
+  $scope.countBasket = basketService.basketQuantity();
+  $scope.isBasketShow = false;
+
+  /**
+   * Pick up the basket update event from the modal
+   */
+  $scope.$on('basketUpdate', function(event, data) { $scope.countBasket = data; });
+
+  /**
+   * Show the basket modal on request
+   */
+  $scope.showBasket = function () {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'basket/basketModal.html',
+      controller: 'BasketCtrl',
+      size: 'lg',
+      scope: $scope,
+      resolve: {
+        countBasket: function () {
+          return $scope.countBasket;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      console.log('Modal dismissed at: ' + new Date());
+    });
+  };
+
+
+
+  /**
+   * Add an item to the basket
+   * @type {Object|Array}
+   */
+  $scope.addToBasket = function(termId, termName){
+
+    var basketItem = {termId: termId, name: termName};
+    basketService.addBasketItem(basketItem);
+    $scope.countBasket = basketService.basketQuantity();
+
+  };
+
 });
