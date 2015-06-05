4fd5fa4483d467b1a3503dabd74c369476bcd8e5
TonyWardell <tonyandamie@yahoo.co.uk>
2015-06-05 15:38:33 +0100

    Reformat code.

--- app/search/searchResult.js
+++ app/search/searchResult.js
@@ -106,59 +106,5 @@ app.controller('SearchResultCtrl', function($scope,  $location, searchfull) {
     return newText;
   }
 
-  //Now do search for this - old code
-  //search.query({query : searchTerm}, function(result){
-  //  $scope.matches=[];
-  //
-  //  console.log("found suggested values data ", result);
-  //  console.log("new value ", searchTerm);
-  //
-  //  //Make the search value lower case so it matches target
-  //  var newValueLc=searchTerm.toLowerCase();
-  //
-  //
-  //  for(var k=0; k<result.go.length; k++){
-  //
-  //    console.log("content of go", result.go[k]);
-  //    console.log("length of return", result.go.length)
-  //
-  //    var aMatch = {};
-  //    aMatch.id = result.go[k].id;
-  //
-  //    //if comment exists look for search value
-  //    if(result.go[k].comment != undefined) {
-  //      console.log("content of comment", result.go[k].comment);
-  //
-  //      //save the comment to an all lowercase
-  //      var target = result.go[k].comment.toLowerCase();
-  //
-  //      var loc = target.indexOf(newValueLc);
-  //      if (loc > -1) {
-  //        aMatch.match = result.go[k].comment.substring(loc - 10, loc + 10);
-  //        aMatch.text = result.go[k].comment;
-  //        $scope.matches[k] = aMatch;
-  //      }
-  //    }
-  //
-  //    if(result.go[k].definition != undefined) {
-  //      console.log("content of definition", result.go[k].definition);
-  //
-  //      //save the comment to an all lowercase
-  //      var target = result.go[k].definition.toLowerCase();
-  //
-  //      var loc = target.indexOf(newValueLc);
-  //      console.log("Definition loc=", loc);
-  //      if (loc > -1) {
-  //        aMatch.match = result.go[k].definition.substring(loc - 10, loc + 10);
-  //        aMatch.text = result.go[k].definition;
-  //        $scope.matches[k] = aMatch;
-  //      }
-  //    }
-  //  }
-  //
-  //  console.log("Populated matches", $scope.matches);
-  //
-  //});
-
 
 });
