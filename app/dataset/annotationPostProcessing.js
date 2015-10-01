9038cf44bc9fae6c938829859992cbe953a50297
TonyWardell <tonyandamie@yahoo.co.uk>
2015-10-01 10:35:28 +0100

    Take out the in-basket method calls atm as they make the page unresponsive. Fix the save of the term aspect to the basket. Use the quickHelperService to lookup the aspect code.

--- app/dataset/annotationPostProcessing.js
+++ app/dataset/annotationPostProcessing.js
@@ -1,7 +1,8 @@
 /**
  * Created by twardell on 03/03/2015.
  */
-app.controller('AnnotationPostProcessingCtrl', function($scope, $modal, basketService, annotationPostProcessing, term) {
+app.controller('AnnotationPostProcessingCtrl', function($scope, $modal, basketService, annotationPostProcessing, term,
+                                                        quickGOHelperService) {
 
   $scope.annotationPostProcessing ={};
   $scope.myPromise = annotationPostProcessing.query({});
@@ -20,17 +21,7 @@ app.controller('AnnotationPostProcessingCtrl', function($scope, $modal, basketSe
     term.query({termId : termId}, function(termData){
       //console.log("[annotationBlacklist] termData");
 
-      var aspect = '';
-      if(termData.aspectDescription == "Cellular Component"){
-        aspect = 'C';
-      }
-      if(termData.aspectDescription == "Molecular Function"){
-        aspect = 'F';
-      }
-      if(termData.aspectDescription == "Biological Process"){
-        aspect = 'P';
-      }
-
+      var aspect =  quickGOHelperService.toAspectCode(termData.aspectDescription);
       var basketItem = {termId: termId, name: termName, aspect: aspect};
 
       basketService.addBasketItem(basketItem);
@@ -46,25 +37,31 @@ app.controller('AnnotationPostProcessingCtrl', function($scope, $modal, basketSe
 
 
   /**
+   * TODO CURRENTLY COMMENTED OUT AS THERE ARE TOO MANY GO IDS ON SCREEN TO PROCESS ATM
    * Remove item from the basket
    * @type {Object|Array}
    */
-  $scope.removeFromBasket = function(termId){
-    console.log(basketService.removeBasketItemById(termId));
-    $scope.$emit('basketUpdate', basketService.basketQuantity());
+  //$scope.removeFromBasket = function(termId){
+  //  console.log(basketService.removeBasketItemById(termId));
+  //  $scope.$emit('basketUpdate', basketService.basketQuantity());
+  //
+  //};
 
-  };
+
 
   /**
+   * TODO CURRENTLY COMMENTED OUT AS THERE ARE TOO MANY GO IDS ON SCREEN TO PROCESS ATM
    * Check if the go term is in the basket
    * @type {Object|Array}
    */
-  $scope.isInBasket = function(termId){
-    //console.log("Testing to see if this is in the basket", termId);
+  //$scope.isInBasket = function(termId){
+  //  //console.log("Testing to see if this is in the basket", termId);
+  //
+  //  var isInBasket = basketService.containsGoTerm(termId);
+  //  //console.log("is this item in the basket", isInBasket);
+  //
+  //  return !isInBasket;
+  //};
+  //
 
-    var isInBasket = basketService.containsGoTerm(termId);
-    //console.log("is this item in the basket", isInBasket);
-
-    return !isInBasket;
-  };
 });
