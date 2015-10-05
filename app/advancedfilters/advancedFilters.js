3813c63ba7f099c9b84b9857b671b5c6502fd703
TonyWardell <tonyandamie@yahoo.co.uk>
2015-10-05 14:20:45 +0100

    Change advanced filters so that filter values are a) always loaded into the advanced filter modal dialogue b) Deselecting a value in the advanced filter m.d. will remove the filter from the list.

--- app/advancedfilters/advancedFilters.js
+++ app/advancedfilters/advancedFilters.js
@@ -34,8 +34,9 @@ app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance, $modal, $
   $scope.advancedFilters.text.ecoID="";
 
   //Setup the fixed eco codes
-  $scope.ecocode0000352='ECO:0000352'
-  $scope.ecocode0000269='ECO:0000269';
+  //$scope.ecocode0000352='ECO:0000352'
+  //$scope.ecocode0000269='ECO:0000269';
+  $scope.evidencesArray = [];
 
   $scope.advancedFilters.text.ecoTermUse="ancestor";
 
@@ -52,6 +53,7 @@ app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance, $modal, $
   $scope.mostCommonTaxonomies = hardCodedDataService.getMostCommonTaxonomies();
   $scope.referenceList = hardCodedDataService.getFilterReferences();
   $scope.qualifiers = hardCodedDataService.getQualifiers();
+  $scope.geneProductSets =  hardCodedDataService.getGeneProductSets();
 
 
 
@@ -75,36 +77,47 @@ app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance, $modal, $
 
     //Load filters containing evidenceTypes only after evidence types have been loaded.
     //todo replace this with loading the evidenceTypes only once
+
+    var ecocode0000352 = {ecoTerm:'ECO:0000352', evidenceKey: 'IMP,IGI,IPI,IDA,IEP,EXP,ISS,TAS,NAS,ND,IC,RCA,IBA,IBD,IKR,IRD,ISA,ISM,ISO,IGC', evidence:'Manual Experimental'};
+    $scope.evidencesArray.push(ecocode0000352);
+
+    var ecocode0000269 = {ecoTerm:'ECO:0000269', evidenceKey: 'IDA,IMP,IPI,IGI,IEP,EXP', evidence:'Manual Experimental'};
+    $scope.evidencesArray.push(ecocode0000269);
+
+    for (i = 0; i < $scope.evidenceTypes.length; i++) {
+
+      $scope.evidencesArray.push({ecoTerm:$scope.evidenceTypes[i].ecoTerm, evidenceKey: $scope.evidenceTypes[i].evidenceKey, evidence:$scope.evidenceTypes[i].evidence});
+
+    }
+
+    console.log("Got evidence types, found", $scope.evidencesArray);
+
     var i;
+
+  //Set to true any evidence for which a filter has been created
     for (i = 0; i < filters.length; i++) {
 
       //Evidence
       if (filters[i].type == 'ecoID') {
 
-        if(filters[i].value=='ECO:0000352'){
-          $scope.advancedFilters.boolean.ecoID[$scope.ecocode0000352] = filters[i].value;
-        }
-
-        if(filters[i].value=='ECO:0000269'){
-          $scope.advancedFilters.boolean.ecoID[$scope.ecocode0000269] = filters[i].value;
-        }
 
 
         //requested taxon in hardcoded list, then populate boolean otherwise populate text
         var ecoCounter;
         var commonEco = false;
-        for (ecoCounter = 0; ecoCounter < $scope.evidenceTypes.length; ecoCounter++) {
+        for (ecoCounter = 0; ecoCounter < $scope.evidencesArray.length; ecoCounter++) {
 
-          console.log("Iterating evidence types, found", $scope.evidenceTypes[ecoCounter].ecoTerm);
+          console.log("Iterating evidence types, found", $scope.evidencesArray[ecoCounter].ecoTerm);
 
-          if ($scope.evidenceTypes[ecoCounter].ecoTerm == filters[i].value) {
+          if ($scope.evidencesArray[ecoCounter].ecoTerm == filters[i].value) {
             console.log("Loading evidence with", filters[i].value);
-            $scope.advancedFilters.boolean.ecoID[filters[i].value] = filters[i].value;
+            $scope.advancedFilters.boolean.ecoID[filters[i].value] = true;
             commonEco = true;
           }
         }
 
-        if (!commonEco && filters[i].value!='ECO:0000352' && filters[i].value!='ECO:0000269') {
+        //if (!commonEco && filters[i].value!='ECO:0000352' && filters[i].value!='ECO:0000269') {
+        if (!commonEco) {
           $scope.advancedFilters.text.ecoID += filters[i].value + '\n';
         }
         continue;
@@ -158,11 +171,11 @@ app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance, $modal, $
 
 
   var filters = filteringService.getFilters();
-  console.log("Loading filters to front end", filters);
+  //console.log("Loading filters to front end", filters);
 
  var i;
   for (i = 0; i < filters.length; i++) {
-  console.log("going through filters", filters[i]);
+  //console.log("going through filters", filters[i]);
 
 
     //Gene Product ID
@@ -171,10 +184,16 @@ app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance, $modal, $
         continue;
     }
 
+    //Gene Product Set
+    if(filters[i].type == 'gpSet'){
+      $scope.advancedFilters.boolean.gpSet[filters[i].value]= true;
+      continue;
+    }
+
 
     //Qualifier
     if(filters[i].type == 'qualifier'){
-      $scope.advancedFilters.boolean.qualifier[filters[i].value] = filters[i].value;
+      $scope.advancedFilters.boolean.qualifier[filters[i].value] = true;
       continue;
     }
 
@@ -184,11 +203,18 @@ app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance, $modal, $
 
       //Is the goID in the basket? if so show its selected.
       var basketCounter;
+      var usedGoID = false;
       for(basketCounter=0; basketCounter<$scope.basketItems.length; basketCounter++){
         if($scope.basketItems[basketCounter].termId == filters[i].value ){
-          $scope.advancedFilters.boolean.goID[$scope.basketItems[basketCounter].termId]=filters[i].value;
-          continue;
+          $scope.advancedFilters.boolean.goID[$scope.basketItems[basketCounter].termId]=true;
+          usedGoID = true;
+          break;
         }
+      }
+
+      //If this filter value has been declared as a basket term then there is no need to continue.
+      if(usedGoID){
+        continue;
       }
 
       //If the goTerm is not in the basket, then it must be in the text box
@@ -223,7 +249,7 @@ app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance, $modal, $
     //  var commonEco=false;
     //  for (ecoCounter = 0; ecoCounter <  $scope.evidenceTypes.length; ecoCounter++){
     //    if( $scope.evidenceTypes[ecoCounter].ecoTerm == filters[i].value){
-    //      $scope.advancedFilters.boolean.ecoID[filters[i].value] = filters[i].value;
+    //      $scope.advancedFilters.boolean.ecoID[filters[i].value] = true;
     //      commonEco=true;
     //    }
     //  }
@@ -237,23 +263,29 @@ app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance, $modal, $
 
     //Reference
     if(filters[i].type == 'reference'){
-      $scope.advancedFilters.boolean.reference[filters[i].value] = filters[i].value;
+      $scope.advancedFilters.boolean.reference[filters[i].value] = true;
       continue;
     }
 
     if(filters[i].type == 'with'){
-      $scope.advancedFilters.boolean.with[filters[i].value] = filters[i].value
+      $scope.advancedFilters.boolean.with[filters[i].value] = true;
       continue;
     }
 
+    console.log("[advancedFilters.js] Testing filters to see if it contains taxon");
+    //advancedFilters.boolean.taxon[taxon.taxId]
     if(filters[i].type == 'taxon'){
+
+      console.log("[advancedFilters.js] Taxon found.");
 
       //requested taxon in hardcoded list, then populate boolean otherwise populate text
       var taxCounter;
       var commonTax=false;
       for (taxCounter = 0; taxCounter < $scope.mostCommonTaxonomies.length; taxCounter++){
         if($scope.mostCommonTaxonomies[taxCounter].taxId == filters[i].value){
-          $scope.advancedFilters.boolean.taxon[filters[i].value] = filters[i].value;
+          //$scope.advancedFilters.boolean.taxon[filters[i].value] = filters[i].value;
+          $scope.advancedFilters.boolean.taxon[filters[i].value] = true;
+          console.log("[advancedFilters.js] setting taxon on ", $scope.advancedFilters.boolean.taxon[filters[i].value]);
           commonTax=true;
         }
       }
@@ -265,7 +297,7 @@ app.controller('AdvancedFiltersCtrl', function($scope, $modalInstance, $modal, $
     }
 
     if(filters[i].type == 'assignedby'){
-      $scope.advancedFilters.boolean.assignedby[filters[i].value] = filters[i].value;
+      $scope.advancedFilters.boolean.assignedby[filters[i].value] = true;
       continue;
     }
 
