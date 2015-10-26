27602eb9a617259c6843ae4801551dcff24f94f3
Xavier Watkins <xavier.watkins@gmail.com>
2015-10-26 17:15:02 +0000

    Basket is now handled by directive

--- app/scripts/services/basketService.js
+++ app/scripts/services/basketService.js
@@ -13,24 +13,15 @@ basketModule.factory('basketService', function($cookieStore) {
    * Add an item to the basket
    */
   basketList.addBasketItem = function (basketItem) {
-    //console.log("basket item is ", basketItem);
-
     var items = $cookieStore.get('uk.ac.ebi.quickgo.basket') || [] ;
-    //items[items.length] = basketItem;
-
     //check the item isn't already isn't in the basket, and if it is don't add it.
-    if(!this.containsItem(basketItem)){
-
-      //console.log("current contents of basket",items);
-
+    if(!this.containsGoTerm(basketItem)){
       items.push(basketItem);
       $cookieStore.put('uk.ac.ebi.quickgo.basket', items);
       return items.length;
     }else{
       //console.log("item already exists in the basket");
     }
-
-
   }
 
 
@@ -38,10 +29,6 @@ basketModule.factory('basketService', function($cookieStore) {
    * Refresh the basket completely
    */
   basketList.refreshBasket = function (newBasketList) {
-    //var items = $cookieStore.get('uk.ac.ebi.quickgo.basket') || [] ;
-    //items[items.length] = basketItem;
-    //console.log(items);
-    //items.push(basketItem);
     $cookieStore.put('uk.ac.ebi.quickgo.basket', newBasketList);
   }
 
@@ -60,56 +47,11 @@ basketModule.factory('basketService', function($cookieStore) {
    * @param basketItem
    */
   basketList.removeBasketItem = function (basketItem) {
-    console.log("BASKET SERVICE: removeBasketItem");
-
-    //console.log("Remove from cookie",basketItem);
-    var basketLen = -1;
-
     var items = $cookieStore.get('uk.ac.ebi.quickgo.basket') || []  ;
-    //console.log("items:: ", items);
-
-    var i;
-    for (i = 0, basketLen = items.length; i < basketLen; i++) {
-
-      //console.log("testing basket item ", i);
-
-      if (items[i].termId == basketItem.termId) {
-        items.splice(i, 1);
-        basketLen--;
-      }
-      $cookieStore.put('uk.ac.ebi.quickgo.basket', items);
-    }
+    items.splice(items.indexOf(basketItem), 1);
+    $cookieStore.put('uk.ac.ebi.quickgo.basket', items);
     return items.length;
   }
-
-
-  /**
-   * Remove an item from the basket
-   * @param basketItem
-   */
-  basketList.removeBasketItemById = function (goId) {
-    console.log("BASKET SERVICE: removeBasketItem");
-
-    //console.log("Remove from cookie",basketItem);
-    var basketLen = -1;
-
-    var items = $cookieStore.get('uk.ac.ebi.quickgo.basket') || []  ;
-    //console.log("items:: ", items);
-
-    var i;
-    for (i = 0, basketLen = items.length; i < basketLen; i++) {
-
-      //console.log("testing basket item ", i);
-
-      if (items[i].termId == goId) {
-        items.splice(i, 1);
-        basketLen--;
-      }
-      $cookieStore.put('uk.ac.ebi.quickgo.basket', items);
-    }
-    return items.length;
-  }
-
 
 
   /*
@@ -135,47 +77,12 @@ basketModule.factory('basketService', function($cookieStore) {
 
   /**
    *
-   * @param searchGoId
-   * @returns {boolean}
-   */
-  basketList.containsItem = function (searchGoId){
-    //console.log("does the basket contain? ", searchGoId);
-    var items = $cookieStore.get('uk.ac.ebi.quickgo.basket') || []  ;
-    //console.log("size of cookie store is ", items.length);
-    var basketLen = -1;
-    var i;
-    for (i = 0, basketLen = items.length; i < basketLen; i++) {
-      //console.log("Have found in the cookie store ", items[i]);
-      if (searchGoId.termId == items[i].termId) {
-        //console.log("Found in cookie list");
-        return true;
-      }
-    }
-    //console.log("Not found in cookie list");
-    return false;
-  }
-
-
-  /**
-   *
-   * @param searchGoId
+   * @param termId
    * @returns {boolean}
    */
   basketList.containsGoTerm = function (termId){
-    //console.log("does the basket contain? ", termId);
     var items = $cookieStore.get('uk.ac.ebi.quickgo.basket') || []  ;
-    //console.log("size of cookie store is ", items.length);
-    var basketLen = -1;
-    var i;
-    for (i = 0, basketLen = items.length; i < basketLen; i++) {
-      //console.log("Have found in the cookie store ", items[i]);
-      if (termId == items[i].termId) {
-        //console.log("Found in cookie list");
-        return true;
-      }
-    }
-    //console.log("Not found in cookie list");
-    return false;
+    return items.indexOf(termId) > -1;
   }
 
 
