8985ff21a4c14674ac917d8e7056680fe458924d
Xavier Watkins <xavier.watkins@gmail.com>
2015-12-10 16:22:44 +0000

    Unrequited dependency in annotation post processing controller

--- app/dataset/annotationPostProcessing.js
+++ app/dataset/annotationPostProcessing.js
@@ -1,14 +1,13 @@
 /**
  * Created by twardell on 03/03/2015.
  */
-app.controller('AnnotationPostProcessingCtrl', function($scope, basketService, annotationPostProcessing, term,
+app.controller('AnnotationPostProcessingCtrl', function($scope, basketService, annotationPostProcessing,
                                                         quickGOHelperService) {
 
   $scope.annotationPostProcessing ={};
   $scope.myPromise = annotationPostProcessing.query({});
   $scope.myPromise.$promise.then(function (data) {
     $scope.annotationPostProcessing = data;
-    console.log("Annotation post processing", $scope.annotationPostProcessing);
   });
 
 });
