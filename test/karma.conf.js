ab903595c95b3bb3a06303768cc7f99c345a163c
Xavier Watkins <xavier.watkins@gmail.com>
2016-03-01 11:56:06 +0000

    Revert

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -91,21 +91,5 @@ module.exports = function(config) {
     // },
     // URL root prevent conflicts with the site root
     // urlRoot: '_karma_'
-    var configuration = {
-      // other things
-
-      customLaunchers: {
-        Chrome_travis_ci: {
-          base: 'Chrome',
-          flags: ['--no-sandbox']
-        }
-      },
-    };
-
-    if (process.env.TRAVIS) {
-      configuration.browsers = ['Chrome_travis_ci'];
-    }
-
-    config.set(configuration);
   });
 };
