8767486efda2da49032a765ceedc18f8f1f5578d
Xavier Watkins <xavier.watkins@gmail.com>
2016-07-06 13:40:35 +0100

    Adding debugging to identify prod issue

--- app/advancedfilters/advancedFilters.js
+++ app/advancedfilters/advancedFilters.js
@@ -18,6 +18,7 @@ app.controller('AdvancedFiltersCtrl', function($scope, $rootScope, $routeParams,
     });
 
     angular.forEach($routeParams, function(val, type) {
+      console.log(val, type);
         if(type === 'id') {
           var isGoTerm = val.indexOf("GO");
           if(isGoTerm >= 0) {
