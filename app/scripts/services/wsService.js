e1695fd23a85f5a4c2bde4b7bcf93c665983f5c3
Xavier Watkins <xavier.watkins@gmail.com>
2016-07-22 13:47:18 +0100

    File now compliant with JSHint

--- app/scripts/services/wsService.js
+++ app/scripts/services/wsService.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var wsService = angular.module('quickGoFeApp.wsService', ['ngResource']);
 
 
@@ -27,15 +29,15 @@ wsService.factory('termService', ['$http', 'ENV', function($http, ENV){
       getBlacklist : function(termId) {
       return $http.get(ENV.apiEndpoint+'/term/' + termId + '/blacklist');
     }
-  }
+  };
 }]);
 
 wsService.factory('stringService', [function(){
   return {
     getTextareaItemsAsArray : function(str) {
-      return _.uniq(str.replace( /\n/g, " " ).split(/[\s,]+/));
+      return _.uniq(str.replace( /\n/g, ' ').split(/[\s,]+/));
     }
-  }
+  };
 }]);
 
 wsService.factory('searchService', ['$http', 'ENV', function($http, ENV){
@@ -64,7 +66,7 @@ wsService.factory('searchService', ['$http', 'ENV', function($http, ENV){
             }
           });
       },
-      findPublications: function(searchTerm, limit) {
+      findPublications: function() {
         //TODO
       },
       findAnnotationsForTerm: function(searchTerm) {
@@ -76,7 +78,7 @@ wsService.factory('searchService', ['$http', 'ENV', function($http, ENV){
           },
           data: {
             list: [{
-              type: "goID",
+              type: 'goID',
               value: searchTerm
             }]
           }
@@ -92,7 +94,7 @@ wsService.factory('searchService', ['$http', 'ENV', function($http, ENV){
           },
           data: {
             list: [{
-              type: "ecoID",
+              type: 'ecoID',
               value: searchTerm
             }]
           }
@@ -108,14 +110,14 @@ wsService.factory('searchService', ['$http', 'ENV', function($http, ENV){
           },
           data: {
             list: [{
-              type: "gpID",
+              type: 'gpID',
               value: searchTerm
             }]
           }
         };
         return $http(request);
       }
-  }
+  };
 }]);
 
 wsService.factory('dbXrefService', ['$http', '$location', function($http, $location){
@@ -141,14 +143,14 @@ wsService.factory('dbXrefService', ['$http', '$location', function($http, $locat
       }
     }
   };
-}])
+}]);
 
 wsService.factory('olsService', ['$http', function($http) {
   return {
     getTermName: function(xref) {
       return $http.get('http://www.ebi.ac.uk/ols/api/ontologies/' + xref.db.toLowerCase() + '/terms/' + 'http%253A%252F%252Fpurl.obolibrary.org%252Fobo%252F' + xref.db + '_' + xref.id);
     }
-  }
+  };
 }]);
 
 wsService.factory('annotationUpdates', ['$resource', 'ENV', function($resource, ENV){
