68cfbc1cc5c98a009fca671605e433e296e64a17
ljgarcia <ljgarcia@ebi.ac.uk>
2016-07-25 16:29:27 +0100

    Add missing semicolons.

--- app/directives/facets.js
+++ app/directives/facets.js
@@ -14,16 +14,16 @@ angular
 
         $scope.getFilterUrl = function(field, category) {
           return (($scope.filters && $scope.filters.length > 0) ? $scope.filters + ',' : '') + field + ':' + category;
-        }
+        };
 
         $scope.isSelected = function(field, category) {
           return $scope.filters.indexOf(field + ':' + category) > -1;
-        }
+        };
 
         $scope.removeFilterUrl = function(field, category) {
 					var filters = _.filter($scope.filters.split(','), function(filter){
 						return !filter.startsWith(field);
-					})
+					});
 					return filters.join();
         }
 
