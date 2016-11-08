13841f17d7788b126a9d42103b24230fa706971f
Xavier Watkins <xavier.watkins@gmail.com>
2016-11-08 15:05:40 +0000

    Display chart inline for now

--- app/directives/chart.js
+++ app/directives/chart.js
@@ -1,27 +1,28 @@
 angular
-	.module('quickGoFeApp')
-	.directive('chartIcon', ['$http','chartService', function($http, chartService) {
-		return {
-			restrict: 'E',
-			scope: {
-				ids: '='
-			},
-			templateUrl: 'directives/chart.html',
-			link: function(scope) {
+  .module('quickGoFeApp')
+  .directive('chartIcon', ['$http', 'chartService', function ($http, chartService) {
+    return {
+      restrict: 'E',
+      scope: {
+        ids: '='
+      },
+      templateUrl: 'directives/chart.html',
+      link: function (scope) {
         scope.showGraph = false;
-        scope.showChart = function() {
-        //TODO check if GO or ECO 
+        scope.showChart = function () {
+          //TODO check if GO or ECO 
           var chartPromise = chartService.getGOChart(scope.ids);
-            
-          chartPromise.then(function(d){
+
+          chartPromise.then(function (d) {
             scope.img = d.data;
-            
+
             //TODO use a modal when moved to Foundation
             scope.showGraph = true;
           });
-          
-//          chartService.getGOImageMap(scope.ids).then(function(d){console.log(d)});
+          chartService.getGOImageMap(scope.ids).then(function (d) {
+            console.log(d)
+          });
         }
-			}
-		};
+      }
+    };
 	}]);
