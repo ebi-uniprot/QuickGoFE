30458508651620cc1050e24bd1b0a38529bd25aa
ljgarcia <leylajael@gmail.com>
2016-11-14 15:57:59 +0000

    Add validation and redirection to taxon filter.
    * Verifies one by one the added taxa:
    ** If not valid, not added, no warning/error reported
    ** If valid but replaced, replaces added value with redirected value
    ** If valid but non existent, removes added value
    ** If valid and existent, adds added value

--- app/advancedfilters/taxonFilter.js
+++ app/advancedfilters/taxonFilter.js
@@ -35,30 +35,50 @@ app.controller('taxonFilter', function($scope, hardCodedDataService,
     return _.pluck(_.filter($scope.taxaIds, 'checked'), 'name');
   };
 
-  var getTaxaInfo = function (taxaIds) {
+  var getTaxaInfo = function (taxaIds, withRedirection) {
     if (taxaIds.length !== 0) {
       var taxonomyPromise = taxonomyService.getTaxa(taxaIds);
       taxonomyPromise.then(function (multipleTaxa) {
         angular.forEach(multipleTaxa.data.taxonomies, function (taxon) {
           $scope.taxaMapping[taxon.taxonomyId] = taxon;
         });
+        angular.forEach(multipleTaxa.data.errors, function (error) {
+          delete $scope.taxaIds[error.requestedId];
+        });
+        if (withRedirection === true) {
+          var redirectedIds = [];
+          angular.forEach(multipleTaxa.data.redirects, function (redirection) {
+            var taxonId = redirection.redirectLocation.substring(redirection.redirectLocation.lastIndexOf('/')+1);
+            redirectedIds.push(taxonId);
+            $scope.taxaIds[taxonId] = {
+              'name': taxonId,
+              'checked': true
+            };
+            delete $scope.taxaIds[redirection.requestedId];
+          });
+          getTaxaInfo(redirectedIds, false);
+        }
       });
     }
   };
 
   $scope.addTaxons = function() {
     var taxons = stringService.getTextareaItemsAsArray($scope.taxonTextArea);
+    var validIds = [];
     angular.forEach(taxons, function(taxonId){
       if($scope.taxaIds[taxonId]) {
         $scope.taxaIds[taxonId].checked = true;
       } else {
-        $scope.taxaIds[taxonId] = {
-          'name': taxonId,
-          'checked': true
-        };
-        getTaxaInfo([taxonId]);
+        if (validationService.validateTaxon(taxonId)) {
+          validIds.push(taxonId);
+          $scope.taxaIds[taxonId] = {
+            'name': taxonId,
+            'checked': true
+          };
+        }
       }
     });
+    getTaxaInfo(validIds, true);
     $scope.taxonTextArea = '';
   };
 
