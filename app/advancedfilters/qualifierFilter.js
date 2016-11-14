bb21be47e946c9746abba29250295ce391ec7e70
Xavier Watkins <xwatkins@ebi.ac.uk>
2016-11-14 11:12:19 +0000

    Fixed qualifier

--- app/advancedfilters/qualifierFilter.js
+++ app/advancedfilters/qualifierFilter.js
@@ -1,3 +1,4 @@
+'use strict';
 app.controller('qualifierFilter', function($scope, hardCodedDataService){
 
   $scope.qualifiers = {};
@@ -5,33 +6,34 @@ app.controller('qualifierFilter', function($scope, hardCodedDataService){
 
   var initQualifiers = function() {
     var checked = [];
-    if($scope.$parent.query.qualifier) {
-      checked = checked.concat($scope.$parent.query.qualifier.split(','))
+    if($scope.query.qualifier) {
+      checked = checked.concat($scope.query.qualifier.split(','));
     }
     angular.forEach(hardCodedDataService.getQualifiers(), function(qualifier) {
       qualifier.checked = _.contains(checked, qualifier.qualifier);
       $scope.qualifiers[qualifier.qualifier] = qualifier;
     });
-  }
+  };
 
   $scope.selectAllNotQualifiers = function () {
-    angular.forEach($scope.qualifiers, function(qualifier) {
-      if(qualifier.qualifier.startsWith('NOT'))
-      $scope.filters.qualifier[qualifier.qualifier] = true;
+    angular.forEach($scope.qualifiers, function(qualifier, name) {
+      if(name.startsWith('NOT')) {
+        $scope.qualifiers[name].checked  = true;
+      }
     });
   };
 
   $scope.apply = function() {
-    $scope.$parent.addToQuery('qualifier', getQuery());
-  }
+    $scope.addToQuery('qualifier', getQuery());
+  };
 
   $scope.reset = function () {
-    $scope.$parent.query.qualifier = '';
-  }
+    $scope.query.qualifier = '';
+  };
 
   var getQuery = function() {
-    return _.pluck(_.filter(_.values($scope.qualifiers), 'checked'), 'name');
-  }
+    return _.pluck(_.filter(_.values($scope.qualifiers), 'checked'), 'qualifier');
+  };
 
   $scope.$on('applyMoreFilters', function(e) {
     $scope.apply();
