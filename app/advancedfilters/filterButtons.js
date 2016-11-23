d725b2d4cd91e8d68987429e5150176233134d91
Xavier Watkins <xwatkins@ebi.ac.uk>
2016-11-23 10:12:08 +0000

    Allowing to set state of filter buttons

--- app/advancedfilters/filterButtons.js
+++ app/advancedfilters/filterButtons.js
@@ -1,8 +1,13 @@
+'use strict';
 app.directive('filterButtons', function() {
   return {
     restrict: 'E',
-    template: '<button class="btn quickgo-btn" type="button" ng-click="reset()">Reset</button>      <button class="btn quickgo-btn" type="button" ng-click="apply()">Apply</button>',
-    link: function($scope, element, attrs) {
+    scope: {
+      active: '@?'
+    },
+    template: '<button class="btn quickgo-btn" type="button" ng-click="reset()">Reset</button>      <button class="btn quickgo-btn" ng-active="{{active}}" type="button" ng-click="apply()">Apply</button>',
+    link: function(scope) {
+      scope.active = scope.active ? scope.active : false;
     }
-  }
-})
+  };
+});
