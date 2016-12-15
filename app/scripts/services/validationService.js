d0a2d46a18d9ed2609ac8c35ead255900710f414
Xavier Watkins <xwatkins@ebi.ac.uk>
2016-12-15 18:45:16 +0100

    Modified regular expression to match with or without database name. Fixes #GOA-2479

--- app/scripts/services/validationService.js
+++ app/scripts/services/validationService.js
@@ -14,7 +14,7 @@ validationModule.factory('validationService', function() {
   };
 
   validationService.validateGeneProduct = function(id) {
-    var matches = (id.match(/^([OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z]([0-9][A-Z][A-Z0-9]{2}){1,2}[0-9])((-[0-9]+)|:PRO_[0-9]{10}|:VAR_[0-9]{6}){0,1}$/) || id.match(/^EBI-[0-9]+$/) || id.match(/^URS[0-9A-F]{10}(_[0-9]+){0,1}$/));
+    var matches = (id.match(/^(\w+:)?([OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z]([0-9][A-Z][A-Z0-9]{2}){1,2}[0-9])((-[0-9]+)|:PRO_[0-9]{10}|:VAR_[0-9]{6}){0,1}$/) || id.match(/^(\w+:)?EBI-[0-9]+$/) || id.match(/^(\w+:)?URS[0-9A-F]{10}(_[0-9]+){0,1}$/));
     return matches;
   };
 
