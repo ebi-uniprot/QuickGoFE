f70218fc5fa628d28f85903d9b48eb8eb4dabd9e
Xavier Watkins <xwatkins@ebi.ac.uk>
2016-12-16 15:12:10 +0100

    Removed log

--- app/search/facetSearch.js
+++ app/search/facetSearch.js
@@ -96,7 +96,6 @@ app.controller('FacetSearchCtrl', function($scope, $location, searchService, $ro
   };
 
   $scope.highlight = function(text) {//TODO check this is still working
-    console.log(text);
     if (!text) {
       return text;
     }
