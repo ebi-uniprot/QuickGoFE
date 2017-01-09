3ce56a76c773e7cb6722d366825ee81b65ff9f83
Xavier Watkins <xwatkins@ebi.ac.uk>
2017-01-09 16:23:16 +0100

    Refactor references filter

--- app/advancedfilters/referencesFilter.js
+++ app/advancedfilters/referencesFilter.js
@@ -1,39 +1,24 @@
 'use strict';
-app.controller('referencesFilter', function($scope, presetsService, stringService){
+app.controller('referencesFilter', function($scope, presetsService, stringService, validationService, filterService){
 
-  $scope.references = {};
+  $scope.references = [];
 
   var getQuery = function() {
-    return _.pluck(_.filter(_.values($scope.references), 'checked'), 'name');
+    return _.pluck(_.filter(_.values($scope.references), 'checked'), 'id');
   };
 
   var initReference = function() {
-    $scope.references = {};
-    //References
-    if($scope.query.reference) {
-      angular.forEach($scope.query.reference.split(','), function(id) {
-        $scope.references[id] = {
-          'name':id,
-          'checked':true
-        };
-      });
-    }
+    $scope.references = filterService.getQueryFilterItems($scope.query.reference);
     presetsService.getPresetsReferences().then(function(resp){
-      angular.forEach(resp.data.references, function(ref){
-        ref.checked = _.contains(_.keys($scope.references), ref.name);
-        $scope.references[ref.name] = ref;
-      });
+      var referencePresetItems = filterService.getPresetFilterItems(resp.data.references, 'name');
+      $scope.references = filterService.mergeRightToLeft($scope.references, referencePresetItems);
     });
   };
 
   $scope.addReferences = function() {
     var refs = stringService.getTextareaItemsAsArray($scope.referenceTextArea);
-    angular.forEach(refs, function(refID){
-      $scope.references[refID] = {
-        'name':refID,
-        checked: true
-      };
-    });
+    var filterItems = filterService.addFilterItems(refs, validationService.validateOther);
+    $scope.references = filterService.mergeRightToLeft(filterItems, $scope.references);
     $scope.referenceTextArea = '';
   };
 
