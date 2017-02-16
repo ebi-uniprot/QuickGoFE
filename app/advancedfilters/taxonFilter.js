22e3b35e73de11d45f54c6f83c1c9d81e8b7282f
ljgarcia <leylajael@gmail.com>
2017-02-16 16:31:43 +0000

    Fix typo and error processing call location.

--- app/advancedfilters/taxonFilter.js
+++ app/advancedfilters/taxonFilter.js
@@ -3,9 +3,16 @@ app.controller('taxonFilter', function($scope, $rootScope, $q, hardCodedDataServ
   stringService, validationService, presetsService, taxonomyService, filterService){
 
   $scope.taxa = [];
+  $scope.alerts = [];
 
   var getQuery = function() {
     return _.pluck(_.filter($scope.taxa, 'checked'), 'id');
+  };
+
+  var processErrors = function() {
+    $rootScope.alerts = [];
+    $rootScope.alerts = $scope.alerts;
+    $scope.alerts = [];
   };
 
   var initTaxons = function(){
@@ -14,6 +21,8 @@ app.controller('taxonFilter', function($scope, $rootScope, $q, hardCodedDataServ
       var presetItems = filterService.getPresetFilterItems(resp.data.taxons, 'name');
       $scope.taxa = filterService.mergeRightToLeft($scope.taxa, presetItems);
       updateTaxonInfo();
+      $scope.alerts = [];
+      $rootScope.alerts = [];
     });
   };
 
@@ -25,10 +34,23 @@ app.controller('taxonFilter', function($scope, $rootScope, $q, hardCodedDataServ
 
   $scope.apply = function() {
     $scope.addToQuery('taxonId', getQuery());
+    $scope.alerts = [];
+    $rootScope.alerts = [];
   };
 
   $scope.addTaxons = function() {
+    $scope.alerts = [];
+
     var taxons = stringService.getTextareaItemsAsArray($scope.taxonTextArea);
+    $scope.alerts = $scope.alerts.concat(_.map(
+      _.filter(taxons, function(id) {return !validationService.validateTaxon(id);}),
+      function(id){
+        return {
+          type: 'alert',
+          msg: '"' + id + '" is not a valid taxon id'
+        };
+      })
+    );
     var items = filterService.addFilterItems(taxons, validationService.validateTaxon);
     $scope.taxa = filterService.mergeRightToLeft(items, $scope.taxa);
     updateTaxonInfo();
@@ -47,11 +69,15 @@ app.controller('taxonFilter', function($scope, $rootScope, $q, hardCodedDataServ
       if(data.data.errors) {
         // remove from list
         var obsoleteIds = _.pluck(data.data.errors, 'requestedId');
-        $rootScope.alerts = _.map(data.data.errors, function(message){
-          return {
-            msg: message.requestedId + ': ' + message.errorMessage
-          };
-        });
+          $scope.alerts = $scope.alerts.concat(_.map(
+          data.data.errors,
+          function(message){
+            return {
+              type: 'warning',
+              msg: 'Id ' + message.requestedId + ': ' + message.errorMessage
+            };
+          })
+        );
         $scope.taxa = $scope.removeTaxIds(obsoleteIds, $scope.taxa);
       }
       if(data.data.redirects) {
@@ -59,20 +85,28 @@ app.controller('taxonFilter', function($scope, $rootScope, $q, hardCodedDataServ
         $scope.taxa = redirectTaxa($scope.taxa, data.data.redirects);
         updateTaxonInfo();
       }
+      processErrors();
     });
   };
-
 
   var redirectTaxa = function(taxaInfo, redirections) {
     var redirectionMap = _.indexBy(redirections, 'requestedId');
     return _.map(taxaInfo, function(d){
       if(redirectionMap[d.id]) {
         var updatedId = redirectionMap[d.id].redirectLocation.substring(redirectionMap[d.id].redirectLocation.lastIndexOf('/')+1);
-        $rootScope.alerts.push({msg: d.id + ' was updated to ' + updatedId});
+        $scope.alerts.push({
+          type: 'warning',
+          msg: 'Taxon ' + d.id + ' was updated to ' + updatedId
+        });
         d.id = updatedId;
       }
       return d;
     });
+  };
+
+  $scope.updateChecked = function() {
+    $scope.alerts = [];
+    $rootScope.alerts = [];
   };
 
   initTaxons();
