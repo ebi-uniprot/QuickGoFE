90b4491b36c4d34e42a4e3e57400f149ac222238
ljgarcia <ljgarcia@ebi.ac.uk>
2017-03-16 16:06:42 +0000

    Use taxonomy service for taxonomy filter

--- app/advancedfilters/taxonFilter.js
+++ app/advancedfilters/taxonFilter.js
@@ -10,10 +10,8 @@ app.controller('taxonFilter', function($scope, $rootScope, $q, hardCodedDataServ
 
   var initTaxons = function(){
     $scope.taxa = filterService.getQueryFilterItems($scope.query.taxonId);
-    presetsService.getPresetsTaxa().then(function(resp){
-      var presetItems = filterService.getPresetFilterItems(resp.data.taxons, 'id');
-      $scope.taxa = filterService.mergeRightToLeft($scope.taxa, presetItems);
-      updateTaxonInfo();
+    taxonomyService.initTaxa($scope.taxa).then(function (data) {
+      $scope.taxa = data;
     });
   };
 
@@ -28,50 +26,9 @@ app.controller('taxonFilter', function($scope, $rootScope, $q, hardCodedDataServ
   };
 
   $scope.addTaxons = function() {
-    var taxons = stringService.getTextareaItemsAsArray($scope.taxonTextArea);
-    var items = filterService.addFilterItems(taxons, validationService.validateTaxon);
-    $scope.taxa = filterService.mergeRightToLeft(items, $scope.taxa);
-    updateTaxonInfo();
-    $scope.taxonTextArea = '';
-  };
-
-  $scope.removeTaxIds = function(idsToRemove, taxa) {
-    return _.filter(taxa, function(d){
-      return !_.contains(idsToRemove, parseInt(d.id));
-    });
-  };
-
-  var updateTaxonInfo = function() {
-    taxonomyService.getTaxa(_.pluck($scope.taxa,'id')).then(function(data){
-      filterService.enrichFilterItemObject($scope.taxa, data.data.taxonomies, 'taxonomyId');
-      if(data.data.errors) {
-        // remove from list
-        var obsoleteIds = _.pluck(data.data.errors, 'requestedId');
-        $rootScope.alerts = _.map(data.data.errors, function(message){
-          return {
-            msg: message.requestedId + ': ' + message.errorMessage
-          };
-        });
-        $scope.taxa = $scope.removeTaxIds(obsoleteIds, $scope.taxa);
-      }
-      if(data.data.redirects) {
-        // update object in list
-        $scope.taxa = redirectTaxa($scope.taxa, data.data.redirects);
-        updateTaxonInfo();
-      }
-    });
-  };
-
-
-  var redirectTaxa = function(taxaInfo, redirections) {
-    var redirectionMap = _.indexBy(redirections, 'requestedId');
-    return _.map(taxaInfo, function(d){
-      if(redirectionMap[d.id]) {
-        var updatedId = redirectionMap[d.id].redirectLocation.substring(redirectionMap[d.id].redirectLocation.lastIndexOf('/')+1);
-        $rootScope.alerts.push({msg: d.id + ' was updated to ' + updatedId});
-        d.id = updatedId;
-      }
-      return d;
+    taxonomyService.addNewTaxa($scope.taxa, $scope.taxonTextArea).then(function(data) {
+      $scope.taxa = data;
+      $scope.taxonTextArea = '';
     });
   };
 
