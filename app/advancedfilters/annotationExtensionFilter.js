6bb602f6ce637dde8239e3e829b9bc322ab081b1
Xavier Watkins <xwatkins@ebi.ac.uk>
2017-03-20 16:06:20 +0000

    Add a total checked to enable/disable Apply button

--- app/advancedfilters/annotationExtensionFilter.js
+++ app/advancedfilters/annotationExtensionFilter.js
@@ -2,10 +2,12 @@
 app.controller('annotationExtensionFilterController', function($scope, $rootScope){
 
   $scope.extension = '';
+  $scope.totalChecked = 0;
 
   var init = function() {
     $rootScope.cleanErrorMessages();
     $scope.extension = $scope.$parent.query.extension ? $scope.$parent.query.extension : '';
+    $scope.totalChecked = $scope.extension.length;
   };
 
   $scope.apply = function() {
@@ -18,6 +20,7 @@ app.controller('annotationExtensionFilterController', function($scope, $rootScop
     $scope.$parent.query.extension = '';
     init();
     $scope.$parent.updateQuery();
+    $scope.totalChecked = 0;
   };
 
   $scope.addComponent = function() {
@@ -26,6 +29,7 @@ app.controller('annotationExtensionFilterController', function($scope, $rootScop
       $scope.relationship = '';
       $scope.db = '';
       $scope.id = '';
+      $scope.totalChecked = component.lenth;
   };
 
   $scope.$on('applyAEFilters', function() {
