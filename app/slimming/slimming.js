d74d009687edefd402533b750f934d1563588456
ljgarcia <leylajael@gmail.com>
2017-03-28 19:24:14 +0100

    Fix adding presets.

--- app/slimming/slimming.js
+++ app/slimming/slimming.js
@@ -73,91 +73,52 @@ app.controller('GOSlimCtrl', function($scope, $location, $q,
     return _.any(_.pluck($scope.basketList, 'selected'));
   };
 
-  var getMergedTermsAndTotal = function(terms, aspectMap) {
-    var mergedTerms = jQuery.extend(true, {}, $scope.selection); //NOTE: use angular's extend, not jQuery
-    var totalCheckedAfterMerge = $scope.getTotalChecked().allAspects;
-
-    angular.forEach(terms, function(goTerm){
-      if (aspectMap && aspectMap[goTerm.aspect]) { //TODO remove when service has correct aspect
-          goTerm.aspect = aspectMap[goTerm.aspect];
-      }
-      totalCheckedAfterMerge += mergedTerms[goTerm.aspect].terms[goTerm.id] ? 0 : 1;
-      mergedTerms[goTerm.aspect].terms[goTerm.id] = goTerm;
-    });
-
-    return {mergedTerms: mergedTerms, totalChecked: totalCheckedAfterMerge}
-  };
-
-  var getEffectiveTotalCheckedAndMergedTerms = function(terms, aspectMap) {
-    /*var mergedTermsAndTotal = getMergedTermsAndTotal(terms, aspectMap);
-    var totalCheckedAfterHandlingError = limitChecker.getTotalCheckedAfterHandlingLimitError($scope.getTotalChecked().allAspects,
-        mergedTermsAndTotal.totalChecked, $scope.uploadLimitGO);
-    if (limitChecker.isTotalDifferent(mergedTermsAndTotal.totalChecked, totalCheckedAfterHandlingError)) {
-      return {mergedTerms: $scope.selection, totalChecked: $scope.getTotalChecked().allAspects};
-    } else {
-      return mergedTermsAndTotal;
-    } */
-  };
-
-  var updateSelectionAndTotal = function(terms, aspectMap) {
-    var effectiveMergedTerms = getEffectiveTotalCheckedAndMergedTerms(terms, aspectMap);
-    $scope.selection = effectiveMergedTerms.mergedTerms;
-    $scope.getTotalChecked().allAspects = effectiveMergedTerms.totalChecked;
-  };
-
-  // Predefined sets
-  $scope.addPredefined = function() {
-    $rootScope.cleanErrorMessages();
-
-    //TODO this is needed as the service currently returns aspect name not id
-    var aspectMap = {};
-    angular.forEach($scope.aspects, function(aspect) {
-      aspectMap[aspect.name] = aspect.id;
-    });
-
-    var terms = $scope.selectedPreDefinedSlimSet.associations;
-    if(!$scope.includeRootTerms) {
-      terms = filterService.removeRootTerms(terms);
-    }
-
-    updateSelectionAndTotal(terms, aspectMap);
-    $scope.selectedPreDefinedSlimSet = '';
-  };
-
   var separateGOTerms = function(terms) {
     var separated = {obsolete: [], active: {}};
     angular.forEach(terms, function(term) {
       if (term.isObsolete) {
-          separated.obsolete.push(term.id);
+        separated.obsolete.push(term.id);
       } else {
-        if (!separated.active[term.aspect]) {
-          separated.active[term.aspect] = [];
-        }
-        separated.active[term.aspect].push(term);
-      }
-    });
+         if (!separated.active[term.aspect]) {
+           separated.active[term.aspect] = [];
+         }
+           separated.active[term.aspect].push(term);
+         }
+      });
     return separated;
   };
 
-  var updateGOSelection = function(termsByAspect) {
+  var updateActivesInGOSelection = function(termsByAspect) {
     angular.forEach($scope.selection, function (aspect, aspectId) {
       aspect.terms = limitChecker.getMergedItems(aspect.terms, termsByAspect[aspectId], $scope.uploadLimitGO);
     });
   };
 
+  var updateGOSelection = function(terms) {
+    var separated = separateGOTerms(terms);
+    $rootScope.stackErrors(separated.obsolete, 'warning', 'is obsolete');
+    updateActivesInGOSelection(separated.active);
+  };
+
+  // Predefined sets
+  $scope.addPredefined = function() {
+    $rootScope.cleanErrorMessages();
+    var terms = $scope.selectedPreDefinedSlimSet.associations;
+    if(!$scope.includeRootTerms) {
+      terms = filterService.removeRootTerms(terms);
+    }
+    updateGOSelection(terms);
+    $scope.selectedPreDefinedSlimSet = '';
+  };
+
   // Own terms     GO:0006915,dsfd,GO:0006916,GO:0004200
   $scope.addOwnTerms = function() {
     $rootScope.cleanErrorMessages();
-
     var goterms = stringService.getTextareaItemsAsArray($scope.slimOwnTerms.toUpperCase());
     var validatedTerms = filterService.validateItems(goterms, validationService.validateGOTerm);
     $rootScope.stackErrors(validatedTerms.invalidItems, 'alert', 'is not a valid GO term id');
-
     termService.getGOTerms(_.pluck(validatedTerms.validItems, 'id')).then(function(d) {
-      var separated = separateGOTerms(d.data.results);
-      $rootScope.stackErrors(separated.obsolete, 'warning', 'is obsolete');
-      updateGOSelection(separated.active);
-      console.log($scope.selection);
+      updateGOSelection(d.data.results);
     });
     $scope.slimOwnTerms = '';
   };
@@ -168,7 +129,7 @@ app.controller('GOSlimCtrl', function($scope, $location, $q,
     var items = _.filter($scope.basketList, function(d) {
       return d.selected;
     });
-    updateSelectionAndTotal(items);
+    //updateSelectionAndTotal(items);
   };
 
   //taxons
