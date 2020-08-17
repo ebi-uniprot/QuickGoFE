774052faffeac2dd15ecf6db83047f2d1e3a0fdb
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2020-08-17 16:35:17 +0100

    probably a fix, needs checking on the dev server

--- app/search/search.js
+++ app/search/search.js
@@ -1,4 +1,5 @@
 'use strict';
 app.controller('SearchCtrl', function($scope,  $location, $routeParams) {
-	$scope.searchTerm = $routeParams.searchTerm;
+	$scope.searchTerm = $routeParams.searchTerm
+    .replace(/\[(.*)\]/, "%5B$1%5D");
 });
